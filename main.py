017254274440ba6eccf08e084e776cc3e0465ce9
Aleksey <savosalex7@gmail.com>
2022-07-08 22:35:26 +0300

    V1

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = telebot.TeleBot('5367324988:AAH-vbdXLimLjzt2r_QGpKTrpjr6AHhfZiA')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    calculation = types.KeyboardButton('Да')
+    markup.add(calculation)
+    mess = f'Здравствуйте <b>{message.from_user.first_name} {message.from_user.last_name}!\n</b>Вас приветствует компания <b>MagiсPrint23!</b>\nМы занимаемся интерьерной печатью и нанесением любых рисунков на любые поверхности. \nХотите расчитать стоимость вашего рисунка?'
+    bot.send_message(message.chat.id, mess, parse_mode='html', reply_markup=markup)
+
+@bot.message_handler(commands=['calculation'])
+def calculation(message):
+    dlina = '<b>Ввведите ширину рисунка</b>'
+    bot.send_message(message.chat.id, dlina, parse_mode='html')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.polling(none_stop=True)
